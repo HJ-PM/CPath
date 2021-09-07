aa3382246fb446d2eb7efeb441ca68f470d86c78
HJ-PM <hpera010@fiu.edu>
2021-09-07 19:47:30 -0400

    View changes, currency formatting, auto tip calculation.

---
+++ Prework_HevertPeralta/SettingsController.swift
@@ -0,0 +1,20 @@
+//
+//  SettingsController.swift
+//  Prework_HevertPeralta
+//
+//  Created by Hevert Peralta on 9/7/21.
+//
+
+import UIKit
+import Foundation
+
+class SettingsController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        
+    }
+    
+}
--- Prework_HevertPeralta/ViewController.swift
+++ Prework_HevertPeralta/ViewController.swift
@@ -10,15 +10,32 @@ import UIKit
 class ViewController: UIViewController {
     
     
+    
     @IBOutlet weak var billAmountTextField: UITextField!
     @IBOutlet weak var tipAmountLabel: UILabel!
     @IBOutlet weak var tipControl: UISegmentedControl!
     @IBOutlet weak var totalLabel: UILabel!
+    let numF = NumberFormatter()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        settingsView.image=UIImage(systemName: "gear")
+        billAmountTextField.keyboardType = .numberPad
+        billAmountTextField.keyboardAppearance = .dark
+        
+        billAmountTextField.becomeFirstResponder()
+        
+        numF.numberStyle = .currency
+        
+        self.title = "Tip Calculator"
+        
     }
+    
+    @IBOutlet weak var settingsView: UIBarButtonItem!
+    
+    
     
     @IBAction func calculateTip(_ sender: Any) {
         
@@ -31,12 +48,16 @@ class ViewController: UIViewController {
         let total = bill + tip
         
         //Update tip label
-        tipAmountLabel.text = String(format: "$%.2f", tip)
+        //tipAmountLabel.text = String(format: "$%.2f", tip)
+        tipAmountLabel.text = numF.string(from: tip as NSNumber)
         
         //Update total amount
-        totalLabel.text = String(format: "$%.2f", total)
+        //totalLabel.text = String(format: "$%.2f", total)
+        totalLabel.text = numF.string(from: total as NSNumber)
         
     }
     
+    
+        
 }
 
